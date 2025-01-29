c8075a463a4ee118e45114e27695507e2cde201c
santten <santtusaaranen@gmail.com>
2025-01-29 15:44:38 +0200

    changed JDK to Corretto 17 for easier JavaFX config, added test application

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,11 @@
+import presentation.StudyShelfApplication;
+
+/**
+ *  The main entry point for the Airport Simulator application.
+ *  This class launches the JavaFX application by calling the start method in SimulatorView.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        StudyShelfApplication.launch(StudyShelfApplication.class);
+    }
+}
