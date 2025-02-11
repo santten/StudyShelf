94a163e0fd4212a7edd9d2538dfaf9d19bb1a439
santten <santtuss@metropolia.fi>
2025-02-11 17:07:26 +0200

    added workspace.xml to .gitignore

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,9 +1,5 @@
 import presentation.view.StudyShelfApplication;
 
-/**
- *  The main entry point for the Airport Simulator application.
- *  This class launches the JavaFX application by calling the start method in SimulatorView.
- */
 public class Launcher {
     public static void main(String[] args) {
         StudyShelfApplication.launch(StudyShelfApplication.class);
