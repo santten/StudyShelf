8d8ff7340c8544eeaceb9797edd43221a158f855
Armas <Armas.Nevolainen@metropolia.fi>
2025-02-11 18:17:20 +0200

    merging main into my branch

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -3,7 +3,7 @@ import presentation.view.StudyShelfApplication;
 import domain.model.*;
 import domain.service.StudyMaterialService;
 import infrastructure.repository.*;
-import presentation.StudyShelfApplication;
+import presentation.view.StudyShelfApplication;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashSet;
