9c4d8897af6344e8d3259603445c2a973e42d963
jiayuez <Jiayue.Zheng@metropolia.fi>
2025-02-19 17:30:38 +0200

    Update Hashing password and JWT

--- src/main/java/domain/service/UserService.java
+++ src/main/java/domain/service/UserService.java
@@ -5,16 +5,20 @@ import domain.model.RoleType;
 import domain.model.User;
 import infrastructure.repository.RoleRepository;
 import infrastructure.repository.UserRepository;
+import java.util.Optional;
 
 public class UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordService passwordService;
+    private final JWTService jwtService;
 
-    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, PasswordService passwordService, JWTService jwtService) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordService = passwordService;
+        this.jwtService = jwtService;
     }
-
 
     public User registerUser(String firstName, String lastName, String email, String password, RoleType roleType) {
         Role role = roleRepository.findByName(roleType);
@@ -24,7 +28,20 @@ public class UserService {
             role = roleRepository.save(role);
         }
 
+        String hashedPassword = passwordService.hashPassword(password);
         User user = new User(firstName, lastName, email, password, role);
         return userRepository.save(user);
+    }
+
+    public String loginUser(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (user != null && passwordService.checkPassword(password, user.getPassword())) {
+            return jwtService.createToken(email);
+        }
+        return null;
+    }
+
+    public boolean isTokenValid(String token) {
+        return jwtService.getEmailFromToken(token) != null;
     }
 }
