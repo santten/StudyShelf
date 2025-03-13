6fb289e30e49a6f3ac5f65fd7148f0bab9d0af7a
Armas <Armas.Nevolainen@metropolia.fi>
2025-03-13 16:50:12 +0200

    Database initializer returned to Launcher

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,7 +1,10 @@
+import infrastructure.config.DatabaseInitializer;
 import presentation.view.StudyShelfApplication;
 
 public class Launcher {
     public static void main(String[] args) {
+        DatabaseInitializer dbInit = new DatabaseInitializer();
+        dbInit.initializeRolesAndPermissions();
         StudyShelfApplication.launch(StudyShelfApplication.class);
     }
 }
