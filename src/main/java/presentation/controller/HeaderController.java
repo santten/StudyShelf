ff12c61d06b739c48745e0a041aa85aed9e36e62
jiayuez <Jiayue.Zheng@metropolia.fi>
2025-05-02 21:08:58 +0300

    H1-3 Minimize users' memory load #1

--- src/main/java/presentation/controller/HeaderController.java
+++ src/main/java/presentation/controller/HeaderController.java
@@ -3,6 +3,7 @@ package presentation.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Tooltip;
 import presentation.enums.ScreenType;
 import presentation.view.SceneManager;
 
@@ -29,6 +30,13 @@ public class HeaderController {
         linkBtnToScreen(btnToSearch, SCREEN_SEARCH);
         linkBtnToScreen(btnToProfile, SCREEN_PROFILE);
         linkBtnToScreen(btnToUpload, SCREEN_UPLOAD);
+
+        linkLogo.setTooltip(new Tooltip("Home"));
+        btnToCourses.setTooltip(new Tooltip("Courses"));
+        btnToSearch.setTooltip(new Tooltip("Search"));
+        btnToProfile.setTooltip(new Tooltip("Profile"));
+        btnToUpload.setTooltip(new Tooltip("Upload"));
+        btnLogOut.setTooltip(new Tooltip("Log Out"));
 
         linkLogo.setOnAction( e -> SceneManager.getInstance().setScreen(SCREEN_HOME));
 
