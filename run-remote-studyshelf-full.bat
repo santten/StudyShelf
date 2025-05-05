2aa17009e7a6224387d974ecae7574c61cb7a53b
Armas <Armas.Nevolainen@metropolia.fi>
2025-05-05 13:14:52 +0300

    .env in docker-compose.yml

--- run-remote-studyshelf-full.bat
+++ run-remote-studyshelf-full.bat
@@ -2,6 +2,16 @@
 echo Starting StudyShelf application with database from Docker Hub...
 echo Make sure VcXsrv (XLaunch) is running!
 
+REM Set environment variables to override persistence.xml
+set DB_HOST=db
+set DB_USER=appuser
+set DB_PASSWORD=password
+set DB_ROOT_PASSWORD=6661507
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+
+echo Using database host: %DB_HOST%
+echo Using downloads directory: %DOWNLOADS_DIR%
+
 REM Pull latest images
 docker-compose pull
 
